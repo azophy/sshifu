25b6197190edadd6226bcba5a12f8adf4147ba42
Abdurrahman Shofy Adianto <azophy@gmail.com>
2026-03-08 16:52:38 +0700

    remove ralph script

--- ralph-once.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-qwen "@PRD.md @PLAN.md @PROGRESS.md \
-1. Read the @PRD.md , @PLAN.md and @PROGRESS.md . \
-2. Find the next incomplete milestone and implement it. \
-3. Commit your changes. \
-4. Update PROGRESS.md with what you did. \
-ONLY DO ONE milestone AT A TIME."
